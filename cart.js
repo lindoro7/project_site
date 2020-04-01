3658a08b3b9064c787d061383803572f18c38b78
lindoro7 <54413736+lindoro7@users.noreply.github.com>
2020-04-01 11:49:01 +0300

    cart update

--- cart.js
+++ cart.js
@@ -4,28 +4,40 @@ let cart = new Vue ({
     image: 'http://via.placeholder.com/72x85',
     url: 'https://raw.githubusercontent.com/lindoro7/project_site/master/db_api/getBasket.json',
     cartShown: false,
+    products: [],
     totalSum: null,
+    
   },
   methods: {   //методы
     getProducts (url) {
       return fetch (url) 
               .then (d => d.json ())
-    }
+    },
+    
       
   },
+
 
   mounted () {
     this.getProducts (this.url)
           .then (data => {
             this.products = data.contents
           })
-          .then (this.products.forEach(element => {
-            this.totalSum += element.quantity * element.price
+          .then (() => {
+            if (!this.products) {
+              this.totalSum = 0
+              document.querySelector(".cartNotEmpty").style.display="none"
+            } else {
+              this.products.forEach(el => {
+                this.totalSum += el.quantity * el.price
+              })
+              console.log(this.totalSum)
+          }
           })
-
           .finally (() => {
             console.log(this.products)
-            console.log(this.totalSum)
           })
+          
+
   }
 })
